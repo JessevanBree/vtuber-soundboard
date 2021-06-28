ccd1c91fe24492dc288c841d248eab5df233b61e
JessevanBree <jessevanbree@live.nl>
2021-06-28 07:17:59 +0200

    added key to map

--- src/components/filter/index.js
+++ src/components/filter/index.js
@@ -17,7 +17,7 @@ function Filter() {
     <>
       <Styled.FilterHeader>Filter</Styled.FilterHeader>
       <Styled.FilterContainer>
-        { channels.map(channel => <Styled.FilterBlock file={channel.file}>
+        { channels.map((channel, i) => <Styled.FilterBlock key={`channel.name_${i}`} file={channel.file}>
           <Styled.FilterTitle>{channel.name}</Styled.FilterTitle>
         </Styled.FilterBlock>)}
       </Styled.FilterContainer>
