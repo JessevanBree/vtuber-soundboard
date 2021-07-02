7162f13cd0ca51fd5501b171a20088d2ec6f937f
JessevanBree <jessevanbree@live.nl>
2021-07-02 22:43:14 +0200

    Revert "Google analytics"
    This reverts commit db4a9ba431e1f9b150f2e0753cc86ac959a0ccef.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-const GA_TRACKING_ID = "G-KX78BDWV2N";
 
 class AppDocument extends Document {
   render() {
@@ -17,23 +16,6 @@ class AppDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500&display=swap" rel="stylesheet"></link>
-
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
         </Head>
         <body>
           <Main />
