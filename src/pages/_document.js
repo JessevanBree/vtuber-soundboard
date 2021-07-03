0b932b68b1676edd8297b96ad86be9c89c5a414c
JessevanBree <jessevanbree@live.nl>
2021-07-03 15:06:23 +0200

    ssr styled components

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 class AppDocument extends Document {
   render() {
@@ -25,6 +26,32 @@ class AppDocument extends Document {
         </body>
       </Html>
     )
+  }
+
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 }
 
